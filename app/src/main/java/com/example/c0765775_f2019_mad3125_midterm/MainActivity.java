7b5f4ef525fbc4d278fa35527c66349204eaf61e
sai teja <saitejasunkari04@gmail.com>
2019-11-30 10:11:03 -0500

    Initial commit

---
+++ app/src/main/java/com/example/c0765775_f2019_mad3125_midterm/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.c0765775_f2019_mad3125_midterm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
