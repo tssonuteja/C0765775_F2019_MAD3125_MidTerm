878bbe7e66f48ba66300cc6023b60d5cc5cf39f6
sai teja <saitejasunkari04@gmail.com>
2019-12-01 13:17:07 -0500

    working on birth date

--- app/src/main/java/com/example/c0765775_f2019_mad3125_midterm/MainActivity.java
+++ app/src/main/java/com/example/c0765775_f2019_mad3125_midterm/MainActivity.java
@@ -2,17 +2,26 @@ package com.example.c0765775_f2019_mad3125_midterm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Calendar;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
 
+    DatePickerDialog picker;
+    EditText eText;
+    Button btnGet;
+    TextView tvw;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +31,35 @@ public class MainActivity extends AppCompatActivity {
         Button btn_check = (Button) findViewById(R.id.btn_check);
         //final MaskEditText edit_s = (MaskEditText) findViewById(R.id.txt_sin);
         final EditText edit_s = (EditText) findViewById(R.id.txtSin);
+        tvw=(TextView)findViewById(R.id.textView1);
+        eText=(EditText) findViewById(R.id.editText1);
+        eText.setInputType(InputType.TYPE_NULL);
+        eText.setOnClickListener(new View.OnClickListener(){
+        @Override
+        public void onClick(View v) {
+            final Calendar cldr = Calendar.getInstance();
+            int day = cldr.get(Calendar.DAY_OF_MONTH);
+            int month = cldr.get(Calendar.MONTH);
+            int year = cldr.get(Calendar.YEAR);
+            // date picker dialog
+            DatePickerDialog picker = new DatePickerDialog(MainActivity.this,
+                    new DatePickerDialog.OnDateSetListener() {
+                        @Override
+                        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                            eText.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year);
+                        }
+                    }, year, month, day);
+            picker.show();
+        }
+    });
+    btnGet=(Button)findViewById(R.id.button1);
+        btnGet.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            tvw.setText("Selected Date: "+ eText.getText());
+        }
+    });
+
 
         btn_check.setOnClickListener(new View.OnClickListener() {
             @Override
