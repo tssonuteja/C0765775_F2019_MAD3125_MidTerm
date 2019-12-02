800690f5a4e71cc70fb5640ea85dca09604d07eb
sai teja <saitejasunkari04@gmail.com>
2019-12-02 18:40:18 -0500

    Working on Second activity

--- app/src/main/java/com/example/c0765775_f2019_mad3125_midterm/SecondActivity.java
+++ app/src/main/java/com/example/c0765775_f2019_mad3125_midterm/SecondActivity.java
@@ -23,11 +23,21 @@ public class SecondActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_second);
+
         fullName = (TextView)findViewById(R.id.txtfullName);
         cAge = (TextView)findViewById(R.id.txtage);
         taxfilingDate = (TextView)findViewById(R.id.taxFilingdate);
         federalTax = (TextView)findViewById(R.id.txtfederalTax);
+        provincialTax = (TextView)findViewById(R.id.txtprovincialTax);
+        carryfrdcpp = (TextView)findViewById(R.id.txtcarryForwardCpp);
+        pEi = (TextView)findViewById(R.id.txteI);
+        carryfrdcpp = (TextView)findViewById(R.id.txtcarryForwardCpp);
+        taxin = (TextView)findViewById(R.id.txtIncome);
+       
+        String fname = getIntent().getStringExtra("fullName");
+        String age = getIntent().getStringExtra("age");
+        String cdate = getIntent().getStringExtra("currentdate");
+
+
     }
 }
